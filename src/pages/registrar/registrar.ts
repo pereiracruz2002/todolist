5944c8a8c9adbbb155156db96c27407caa524550
Flávio <pereiracruz2002@gmail.com>
2018-03-10 08:52:39 -0300

    até a aula 3

---
+++ src/pages/registrar/registrar.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-registrar',
+  templateUrl: 'registrar.html'
+})
+export class Registrar {
+
+  constructor(public navCtrl: NavController) {
+  	//public loginProvider:loginProvider
+
+  }
+
+}
